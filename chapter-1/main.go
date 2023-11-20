93f538fe7f6ecd7a4ff204a56e20adc80bcd10fa
lapeko <lapeko88@gmail.com>
2023-11-20 20:47:13 +0100

    define variables and func

--- chapter-1/main.go
+++ chapter-1/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(123)
+	fmt.Println(123)
+
+	var whatToSay string
+	var num int
+
+	whatToSay = "Goodbye, cruel world !"
+	num = 7
+
+	fmt.Println(whatToSay)
+	fmt.Println("I is set to", num)
+
+	whatWasSaid, otherThing := saySomething()
+	fmt.Println(whatWasSaid, otherThing)
+}
+
+func saySomething() (string, string) {
+	return "Something", "other thing"
 }
