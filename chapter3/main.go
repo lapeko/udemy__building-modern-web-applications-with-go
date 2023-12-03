45656dd6cd5929692ad0db9a80d6b3f88ba3b2cf
lapeko <lapeko88@gmail.com>
2023-12-03 22:05:03 +0100

    add about route

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = ":8080"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, fmt.Sprintf("About page. The sum of 2 and 2 is %d", calculateSum(2, 2)))
+}
+
+func calculateSum(num1, num2 int) int {
+	return num1 + num2
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Hello world"))
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	fmt.Printf("Server is running on port: %s", port)
+	_ = http.ListenAndServe(port, nil)
 }
