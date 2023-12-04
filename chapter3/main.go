81d307f1d5a732cf62644b80e9a4cdc4654e2072
lapeko <lapeko88@gmail.com>
2023-12-04 21:53:12 +0100

    add divide http method

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -15,13 +16,32 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, fmt.Sprintf("About page. The sum of 2 and 2 is %d", calculateSum(2, 2)))
 }
 
+func divideHandler(w http.ResponseWriter, r *http.Request) {
+	a := 10.0
+	b := 2.0
+	result, err := divideNumber(a, b)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, fmt.Sprintf(err.Error()))
+		return
+	}
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f / %f is %f", a, b, result))
+}
+
 func calculateSum(num1, num2 int) int {
 	return num1 + num2
+}
+
+func divideNumber(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("0 is forbidden to use as a divider")
+	}
+	return a / b, nil
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/divide", divideHandler)
 	fmt.Printf("Server is running on port: %s", port)
 	_ = http.ListenAndServe(port, nil)
 }
