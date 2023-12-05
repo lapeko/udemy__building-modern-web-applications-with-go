8e9bcf3155c0ebee2b1abd5eb8e39b1441035486
lapeko <lapeko88@gmail.com>
2023-12-05 21:55:46 +0100

    render html temlates

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,47 +1,32 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const port = ":8080"
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+	renderTemplate(w, "home")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, fmt.Sprintf("About page. The sum of 2 and 2 is %d", calculateSum(2, 2)))
+	renderTemplate(w, "about")
 }
 
-func divideHandler(w http.ResponseWriter, r *http.Request) {
-	a := 10.0
-	b := 2.0
-	result, err := divideNumber(a, b)
+func renderTemplate(w http.ResponseWriter, templateName string) {
+	templates, _ := template.ParseFiles("./templates/" + templateName + ".html")
+	err := templates.Execute(w, nil)
 	if err != nil {
-		_, _ = fmt.Fprintf(w, fmt.Sprintf(err.Error()))
-		return
+		fmt.Printf("Template %s successfully send", templateName)
 	}
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f / %f is %f", a, b, result))
-}
-
-func calculateSum(num1, num2 int) int {
-	return num1 + num2
-}
-
-func divideNumber(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("0 is forbidden to use as a divider")
-	}
-	return a / b, nil
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/divide", divideHandler)
 	fmt.Printf("Server is running on port: %s", port)
 	_ = http.ListenAndServe(port, nil)
 }
