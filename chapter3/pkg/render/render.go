1d508c613f0a281ccbd043c21308b2250796533c
lapeko <lapeko88@gmail.com>
2023-12-09 19:00:00 +0100

    cache templates

--- chapter3/pkg/render/render.go
+++ chapter3/pkg/render/render.go
@@ -6,10 +6,25 @@ import (
 	"text/template"
 )
 
+var templateCache = make(map[string]*template.Template)
+
 func Template(w http.ResponseWriter, templateName string) {
-	templates, _ := template.ParseFiles("./templates/"+templateName+".page.gohtml", "./templates/base.layout.gohtml")
+	templates, _ := makeTemplates(templateName)
 	err := templates.Execute(w, nil)
 	if err != nil {
 		fmt.Printf("Template %s successfully send", templateName)
 	}
+}
+
+func makeTemplates(templateName string) (*template.Template, error) {
+	templates, keyExist := templateCache[templateName]
+	if keyExist {
+		return templates, nil
+	}
+	templates, err := template.ParseFiles("./templates/"+templateName+".page.gohtml", "./templates/base.layout.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	templateCache[templateName] = templates
+	return templates, nil
 }
